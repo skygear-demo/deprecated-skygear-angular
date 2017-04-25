4083331c0ea38309dd08c4708d3bcd5088c5c606
Angular CLI <angular-cli@angular.io>
2017-04-25 03:37:06 +0200

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MyAngularAppPage } from './app.po';
+
+describe('my-angular-app App', () => {
+  let page: MyAngularAppPage;
+
+  beforeEach(() => {
+    page = new MyAngularAppPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
