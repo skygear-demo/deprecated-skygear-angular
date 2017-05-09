ef11c29352844a3a7ea58b4abae81c8274fc3e55
Brian Chan <i@b123400.net>
2017-05-09 23:31:28 +0100

    Add code for creating a new record

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,18 @@ export class AppComponent implements OnInit {
       this.title = "Cannot configure skygear";
     });
   }
+
+  addNewRecord() {
+    this.skygearService.getSkygear()
+    .then(()=> this.skygear.signupAnonymously())
+    .then(()=> {
+      var Note = this.skygear.Record.extend('Note');
+      return this.skygear.publicDB.save(new Note({
+        'content': 'Hello World'
+      }));
+    })
+    .then((record)=> {
+      this.title = "Saved record: " + record.id;
+    });
+  }
 }
